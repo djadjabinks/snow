5e3e7e709574f49f24bac41aa5a7991d12754f68
30padavan10 <pankov.dmitriy@itmh.ru>
2022-02-21 17:34:39 +0500

    __add__

--- snowlab.py
+++ snowlab.py
@@ -394,3 +394,54 @@ class Point:
 pt = Point(1, -2 ,4)
 print(len(pt))
 print(abs(pt))
+
+##########################################################################
+
+# Магические методы __add__, __sub__, __mul__, __truediv__
+
+class Clock:
+    __DAY = 86400 # число секунд в сутках
+
+    def __init__(self, seconds: int):
+        if not isinstance(seconds, int):
+            raise TypeError("Должно быть целое число")
+        self.seconds = seconds % self.__DAY
+
+    def get_time(self):
+        s = self.seconds % 60
+        m = (self.seconds // 60) % 60
+        h = (self.seconds // 3600) % 24
+        return f"{self.__get_formatted(h)}:{self.__get_formatted(m)}:{self.__get_formatted(s)}"
+
+    @classmethod
+    def __get_formatted(cls, x):
+        return str(x).rjust(2, "0")
+
+    def __add__(self, other):
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Правый операнд должен быть числом")
+        sc = other
+        if isinstance(sc, Clock):
+            sc = other.seconds
+        return Clock(self.seconds + sc)
+
+    def __radd__(self, other):  # используется когда число слева от экземпляра класса
+        return self + other     # в данном месте self вызовет __add__ который выполнит сложение
+
+    def __iadd__(self, other):
+        if not isinstance(other, int):
+            raise ArithmeticError("Правый операнд должен быть числом")
+        sc = other
+        if isinstance(sc, Clock):
+            sc = other.seconds
+        self.seconds += sc
+        return self
+
+
+c1 = Clock(2000)
+c2 = Clock(1000)
+c4 = Clock(3000)
+c5 = c1 + c2 + c4
+c3 = 100 + c1
+print(c3.get_time())
+print(c5.get_time())
